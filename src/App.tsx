6e3f1130e56971b4a3a925e3e9494b10ff10ae95
jadedResearcher <gtg287y@gmail.com>
2021-02-02 20:56:46 -0500

    weird ambient

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ import mist1 from './mist1.png';
 import './App.css';
 import ShitpostRoot from "./Shitposting/ShitpostRoot";
 
+function startbgmusic(){
+  console.log("click first");
+  const audioElement = new Audio("http://knucklessux.com/PuzzleBox/SoundFX/weirdambient.mp3");
+  audioElement.volume = 0.1;
+  audioElement.loop = true;
+  audioElement.play();
+  document.removeEventListener("click",startbgmusic);
+}
+
+document.addEventListener('click', startbgmusic);
+
+
 function App() {
   return (
     <div className="App">
