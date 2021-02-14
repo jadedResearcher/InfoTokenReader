553a604afbf4855481fc5e67105d7f5f6e206beb
jadedResearcher <gtg287y@gmail.com>
2021-02-14 18:59:07 -0500

    in theory animations are real

--- src/Shitposting/PictureThought.tsx
+++ src/Shitposting/PictureThought.tsx
@@ -7,7 +7,7 @@ import deep_fry_options from "../utils/DeepFryer";
 
 type ClickableProps = {
   rand: SeededRandom
-  ref: RefObject<HTMLCanvasElement>;
+  ref: RefObject<HTMLDivElement>;
 }
 
 
@@ -35,31 +35,58 @@ type PictureProps = {
 const PictureThought = (props:PictureProps)=> {
   const {src, rand, clickAction, search_term} = props;
   const url=root + picture_thoughts + "/" +search_term + "/" + src;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const onScreenRef = useRef<HTMLDivElement>(null);
+  const canvasElement = document.createElement('canvas');
+  canvasElement.setAttribute("width","400");
+  canvasElement.setAttribute("height","400");
 
 
   
-const deepfriedCanvas = (img: HTMLImageElement, canvas: HTMLCanvasElement) => {
+const deepfriedCanvas = (div: HTMLDivElement, img: HTMLImageElement, canvas: HTMLCanvasElement) => {
   const nextSeed = rand.getRandomNumberBetween(0,1013904223);
   img.crossOrigin = "Anonymous";
   var ctx = canvas.getContext('2d');
-  if(!ctx){
+  const bgElement = document.createElement('canvas');
+  var bgctx = bgElement.getContext('2d');
+  if(!ctx || ! bgctx){
     console.error("Uh. Why isn't there a canvas???");
     return;
   }
   const width = rand.getRandomNumberBetween(100,400);
   const height = rand.getRandomNumberBetween(100,400);
+  
+
+
   canvas.setAttribute("width",`${width}`);
   canvas.setAttribute("height",`${height}`);
+  div.setAttribute("width",`${width}px`);
+  div.setAttribute("height",`${height}px`);
+  bgElement.setAttribute("width",`${width*2}px`);
+  bgElement.setAttribute("height",`${height}px`);
+
+
+
   ctx.drawImage(img, 0, 0,width,height);
   const num_layers = rand.getRandomNumberBetween(1,4);
+
+  //deep fry it once
   for(let i = 0; i<num_layers; i++){
       deep_fry_options[rand.getRandomNumberBetween(0,deep_fry_options.length-1)](canvas);
   }
+  //draw it to the first part of the bg
+  bgctx.drawImage(canvas, 0,0);
+  //deep fry it again
+  for(let i = 0; i<num_layers; i++){
+    deep_fry_options[rand.getRandomNumberBetween(0,deep_fry_options.length-1)](canvas);
+  }
+  //draw it to the second part
+  bgctx.drawImage(canvas, 0,width);
+
+  div.style.backgroundImage = `url(${bgElement.toDataURL()})`;
 }
 
-const updateCanvas =(canvas: HTMLCanvasElement,current_src:string) =>{
-  const ctx = canvas.getContext('2d');
+const updateCanvas =(div: HTMLDivElement, current_src:string) =>{
+  const ctx = canvasElement.getContext('2d');
   if(!ctx){
     return;
   }
@@ -67,22 +94,23 @@ const updateCanvas =(canvas: HTMLCanvasElement,current_src:string) =>{
   var img = new Image();
   img.src = root  + picture_thoughts + "/" + search_term + "/" + current_src;
   img.onload = ()=> {
-      deepfriedCanvas(img, canvas);
-    }
+    deepfriedCanvas(div, img, canvasElement);
+  }
 }
 
 useEffect(() =>{
-    if(canvasRef.current){
-    updateCanvas(canvasRef.current, src);
-    }
-}, [canvasRef, src])
+  if(onScreenRef.current){
+    updateCanvas(div, src);
+  }
+
+}, [src])
 
 
 
 
   return (
     <Fragment>
-      <ClickableBullshit width="400" height="400" ref={canvasRef} className={'pulse_animated'} rand={rand} onClick={()=>{return clickAction()}}/>
+      <ClickableBullshit width="400" height="400" ref={onScreenRef} className={'pulse_animated, frameAnimation'} rand={rand} onClick={()=>{return clickAction()}}/>
     </Fragment>
   );
 }
